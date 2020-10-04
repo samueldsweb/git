27dd1b30c411e769630c56f2865962f86c9df8b3
Samuel Sánchez <samuel.sanchez@destacados.cl>
2020-10-03 22:29:29 -0300

    se realiza typeform y se agrega animacion del scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
 $(function () {
 
-    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $("a").click(function(event){
+        if (this.hash !== "") {
+          event.preventDefault();
     
+          var scroll = this.hash;
+    
+          $("html, body").animate({
+            scrollTop: $(scroll).offset().top
+          }, 800, function(){
+            window.location.hash = scroll;
+          });
+        }
+    });
+
 })
